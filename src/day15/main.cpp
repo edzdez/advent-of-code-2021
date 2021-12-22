5c1725cf632c7150cdd1308b450c35a212490db7
Ethan Zhang <ezsland05@gmail.com>
2021-12-21 18:31:46 -0800

    Added solution for Day 15 part 2

--- src/day15/main.cpp
+++ src/day15/main.cpp
@@ -16,15 +16,17 @@ map<pair<int, int>, int> dijkstra(const vector<vector<int>> &dangerLevels, const
             minDistances[{j, i}] = INT32_MAX;
     minDistances[startPoint] = 0;
 
-    priority_queue<pair<int, pair<int, int>>> pq;
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
     pq.push({0, startPoint});
 
     int dx[]{-1, 1, 0, 0}, dy[]{0, 0, -1, 1};
     while (!pq.empty())
     {
-        auto [_, p] = pq.top();
-        auto [x, y] = p;
+        auto [x, y] = pq.top().second;
         pq.pop();
+
+        if (x == dangerLevels[0].size() - 1 && y == dangerLevels.size() - 1)
+            break;
 
         for (int i = 0; i < 4; ++i)
         {
@@ -37,7 +39,7 @@ map<pair<int, int>, int> dijkstra(const vector<vector<int>> &dangerLevels, const
             if (minDistances[{absX, absY}] > w + minDistances[{x, y}])
             {
                 minDistances[{absX, absY}] = w + minDistances[{x, y}];
-                pq.push({w, make_pair(absX, absY)});
+                pq.push({minDistances[{absX, absY}], make_pair(absX, absY)});
             }
         }
     }
@@ -48,7 +50,41 @@ map<pair<int, int>, int> dijkstra(const vector<vector<int>> &dangerLevels, const
 void part1(const vector<vector<int>> &dangerLevels)
 {
     auto distances = dijkstra(dangerLevels, {0, 0});
-    cout << distances[{dangerLevels[0].size() - 1, dangerLevels.size() - 1}];
+    cout << distances[{dangerLevels[0].size() - 1, dangerLevels.size() - 1}] << '\n';
+}
+
+vector<vector<int>> expand(const vector<vector<int>> &dangerLevels)
+{
+    vector<vector<int>> res;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        for (int y = 0; y < dangerLevels.size(); ++y)
+        {
+            vector<int> line;
+            for (int j = 0; j < 5; ++j)
+            {
+                for (int x = 0; x < dangerLevels[0].size(); ++x)
+                {
+                    auto res = dangerLevels[y][x] + i + j;
+                    while (res > 9)
+                        res -= 9;
+
+                    line.push_back(res);
+                }
+            }
+            res.push_back(line);
+        }
+    }
+
+    return res;
+}
+
+void part2(const vector<vector<int>> &dangerLevels)
+{
+    auto expandedLevels = expand(dangerLevels);
+    auto distances = dijkstra(expandedLevels, {0, 0});
+    cout << distances[{expandedLevels[0].size() - 1, expandedLevels.size() - 1}] << '\n';
 }
 
 int main()
@@ -66,4 +102,5 @@ int main()
     }
 
     part1(dangerLevels);
+    part2(dangerLevels);
 }
